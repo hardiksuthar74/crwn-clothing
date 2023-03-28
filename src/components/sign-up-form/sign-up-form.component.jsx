3c525dbf4b2ad2b07fad2bad23997c96a623c9a1
hardiksuthar74 <hardiksuthar74@gmail.com>
2023-03-28 20:09:59 +0530

    added sign up component, button component

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import FormInput from "../form-input/form-input.component";
+import "./sign-up-form.styles.scss";
+import Button from "../button/button.component";
+
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
+const defaultFormField = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormField);
+  const { displayName, email, password, confirmPassword } = formFields;
+
+  console.log(formFields);
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormField);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("Password do not match");
+      return;
+    }
+
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      await createUserDocumentFromAuth(user, { displayName });
+      resetFormFields();
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        alert("Email already in use");
+      } else {
+        console.log(error);
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Display Name"
+          inputOption={{
+            type: "text",
+            required: true,
+            onChange: handleChange,
+            name: "displayName",
+            value: displayName,
+          }}
+        />
+
+        <FormInput
+          label="Email"
+          inputOption={{
+            type: "email",
+            required: true,
+            onChange: handleChange,
+            name: "email",
+            value: email,
+          }}
+        />
+
+        <FormInput
+          label="Password"
+          inputOption={{
+            type: "password",
+            required: true,
+            onChange: handleChange,
+            name: "password",
+            value: password,
+          }}
+        />
+
+        <FormInput
+          label="Confirm Password"
+          inputOption={{
+            type: "password",
+            required: true,
+            onChange: handleChange,
+            name: "confirmPassword",
+            value: confirmPassword,
+          }}
+        />
+
+        <Button type="submit">Sign up</Button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,38 @@
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
+
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import {
+  // auth,
   signInWithGooglePopup,
-  getUserDocumentFromAuth,
+  // signInWithGoogleRedirect,
+  createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+
+//   useEffect(() => {
+//     async function redirectResultFunction() {
+//       const response = await getRedirectResult(auth);
+//       if (response) {
+//         const userDocRef = await getUserDocumentFromAuth(response.user);
+//       }
+//     }
+//     redirectResultFunction();
+//   }, []);
 
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
-    getUserDocumentFromAuth(user);
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
 
   return (
     <div>
       <h1>Hello SignIn</h1>
-      <button onClick={logGoogleUser}>Sign-In with Google</button>
+      <button onClick={logGoogleUser}>Sign-In with Google Popup</button>
+      <SignUpForm />
+      {/* <button onClick={signInWithGoogleRedirect}>
+        Sign-In with Google Redirect
+      </button> */}
     </div>
   );
 };
